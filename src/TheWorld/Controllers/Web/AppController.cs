5114553b5ce7d3190698d8402c5e5fbf244b77fc
Aleksandr <cushionyten0@gmail.com>
2018-05-19 21:00:10 -0400

    Added Services and Validators

--- src/TheWorld/Controllers/Web/AppController.cs
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -4,16 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TheWorld.Services;
+using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Web {
     public class AppController : Controller {
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        //private IMailService _mailService;
+
+        public AppController (IMailService mailService, IConfigurationRoot config) {
+            _mailService = mailService;
+            _config = config;
+        }
+
         public IActionResult Index () {
             return View ();
         }
         public IActionResult Contact () {
             //throw new InvalidOperationException ("Bad things happen to good developers");
+
             return View ();
         }
+
+        [HttpPost]
+        public IActionResult Contact (ContactViewModel model) {
+            _mailService.SendMail (_config["MailSettings:ToAddress"], model.Email, "From TheWorld", model.Message);
+            return View ();
+
+        }
+
         public IActionResult About () {
             return View ();
         }
--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -5,13 +5,35 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TheWorld.Services;
 
 namespace src {
     public class Startup {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private IHostingEnvironment _env;
+        private IConfigurationRoot _config;
+
+        public Startup (IHostingEnvironment env) {
+            _env = env;
+
+            var builder = new ConfigurationBuilder ()
+                .SetBasePath (_env.ContentRootPath)
+                .AddJsonFile ("config.json")
+                .AddEnvironmentVariables ();
+
+            _config = builder.Build ();
+        }
+
         public void ConfigureServices (IServiceCollection services) {
+            services.AddSingleton (_config);
+
+            if (_env.IsEnvironment ("Development") || _env.IsEnvironment ("Testing")) {
+                services.AddScoped<IMailService, DebugMailService> ();
+            } else {
+                //Implement a real Mail Service
+            }
+
             services.AddMvc ();
         }
 
