851cbc729adeb58dfa8ccc9256682cf333568fbd
Aleksandr <cushionyten0@gmail.com>
2018-05-23 12:28:49 -0400

    Testing Environment Vars in Docker

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,7 @@ source ../deploy-envs.sh
 
 #AWS_ACCOUNT_NUMBER={} set in private variable
 export AWS_ECS_REPO_DOMAIN=$AWS_ACCOUNT_NUMBER.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
-export ASPNETCORE_ENVIRONMENT=$ASPNETCORE_ENVIRONMENT
 
 # Build process
-docker build -t $IMAGE_NAME ../
+docker build -t $IMAGE_NAME ../ --build-arg ASPNETCORE_ENVIRONMENT=$ASPNETCORE_ENVIRONMENT
 docker tag $IMAGE_NAME $AWS_ECS_REPO_DOMAIN/$IMAGE_NAME:$IMAGE_VERSION
