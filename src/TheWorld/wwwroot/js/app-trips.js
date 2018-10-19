17cc2b5dc56f3d0c16d1aa479eab5a2a1076498f
Aleksandr <cushionyten0@gmail.com>
2018-10-18 22:35:58 -0400

    Added Angular Routing

--- src/TheWorld/wwwroot/js/app-trips.js
+++ src/TheWorld/wwwroot/js/app-trips.js
@@ -1,7 +1,17 @@
-//app-trips.js
+// app-trips.js
 (function() {
   "use strict";
 
-  //Creating the Module
-  angular.module("app-trips", []);
+  // Creating the Module
+  angular
+    .module("app-trips", ["simpleControls", "ngRoute"])
+    .config(function($routeProvider) {
+      $routeProvider.when("/", {
+        controller: "tripsController",
+        controllerAs: "vm",
+        templateUrl: "/views/tripsView.html"
+      });
+
+      $routeProvider.otherwise({ redirectTo: "/" });
+    });
 })();
