6f83670a167c7e97f33d7acf7956ad1977b4b953
Aleksandr <cushionyten0@gmail.com>
2018-10-24 11:59:55 -0400

    Implemented Viewing Stops related to a Trip

--- src/TheWorld/wwwroot/js/tripEditorController.js
+++ src/TheWorld/wwwroot/js/tripEditorController.js
@@ -7,12 +7,28 @@
     .module("app-trips")
     .controller("tripEditorController", tripEditorController);
 
-  function tripEditorController($routeParams) {
+  function tripEditorController($routeParams, $http) {
     var vm = this;
 
     vm.tripName = $routeParams.tripName;
     vm.stops = [];
     vm.errorMessage = "";
     vm.isBusy = true;
+
+    $http
+      .get("/api/trips/" + vm.tripName + "/stops")
+      .then(
+        function(response) {
+          //success
+          angular.copy(response.data, vm.stops);
+        },
+        function(err) {
+          //failure
+          vm.errorMessage = "Failed to load Stops";
+        }
+      )
+      .finally(function() {
+        vm.isBusy = false;
+      });
   }
 })();
