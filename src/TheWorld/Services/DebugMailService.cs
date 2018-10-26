658bf1eba42423c3bddc3b016f69f8a1dc065ce5
Aleksandr <cushionyten0@gmail.com>
2018-10-26 17:48:47 -0400

    Added Validation for Adding new Stops

--- src/TheWorld/Services/DebugMailService.cs
+++ src/TheWorld/Services/DebugMailService.cs
@@ -5,9 +5,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TheWorld.Services {
-    public class DebugMailService : IMailService {
-        public void SendMail (string to, string from, string subject, string body) {
+namespace TheWorld.Services
+{
+    public class DebugMailService : IMailService
+    {
+        public void SendMail (string to, string from, string subject, string body)
+        {
             Debug.WriteLine ($"Sending Mail: To: {to} From: {from} Subject: {subject} ");
         }
     }
